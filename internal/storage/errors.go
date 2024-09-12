d52c9170417da7792ceeb62f22adfd8f11c19fa3
dspashintsev <dspashintsev@avito.ru>
2024-09-12 17:07:32 +0300

    ашч

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -10,7 +10,6 @@ var ErrUniqueKeyViolation = errors.New("unique key violation")
 
 func handleErr(err error) error {
 	pgErr, ok := err.(*pq.Error)
-	fmt.Println("CODE: ", pgErr.Code)
 	if ok && pgErr.Code == "23505" {
 		return fmt.Errorf("%w: %v", ErrUniqueKeyViolation, err)
 	}
