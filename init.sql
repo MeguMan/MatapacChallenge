69e18e23775c18c4066af5cd674d3fa5a22a6daa
dspashintsev <dspashintsev@avito.ru>
2024-09-12 17:48:25 +0300

    init fix

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 create table users (
-    tg_id          integer unique primary key,
+    tg_id          bigint  unique primary key,
     tg_username    text    not null,
     sol_public_key text    not null,
-    tg_chat_id     bigint not null,
+    tg_chat_id     bigint  not null,
     created_at  timestamp without time zone default NOW()
 );
