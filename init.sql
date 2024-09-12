1800722bb308b7bc19df049bf50734af1b1fd2a7
dspashintsev <dspashintsev@avito.ru>
2024-09-12 21:19:43 +0300

    test

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 create table users (
     tg_id          bigint  unique primary key,
     tg_username    text    not null,
-    sol_public_key text    not null,
+    sol_public_key text    unique not null,
     created_at  timestamp without time zone default NOW()
 );
