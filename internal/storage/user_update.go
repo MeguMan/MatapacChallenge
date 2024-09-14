e14125482aad42f982a922cd867216b849f2ed19
dspashintsev <dspashintsev@avito.ru>
2024-09-14 15:54:20 +0300

    fix

--- internal/storage/user_update.go
+++ internal/storage/user_update.go
@@ -9,7 +9,7 @@ func (s *Storage) UpdateUser(ctx context.Context, user User) error {
 	query, params, err := sq.Update(usersTableName).
 		SetMap(map[string]interface{}{
 			"sol_public_key": user.SolPublicKey,
-			"attempt":        "attempt + 1",
+			"attempt":        sq.Expr("attempt+1"),
 		}).
 		Where(sq.Eq{"tg_id": user.TgID}).
 		PlaceholderFormat(sq.Dollar).
